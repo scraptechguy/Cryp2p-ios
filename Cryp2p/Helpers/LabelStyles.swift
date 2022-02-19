c95fa73f6417a38d2e520196640a25c05907e195
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-19 18:22:11 +0100

    Create `ButtonLabel` struct

---
+++ Cryp2p/Helpers/LabelStyles.swift
@@ -0,0 +1,19 @@
+//
+//  LabelStyles.swift
+//  Cryp2p
+//
+//  Created by Rostislav Brož on 2/19/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct ButtonLabel: View {
+    // Access data in ContentModel.swift
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        Text("")
+    }
+}
