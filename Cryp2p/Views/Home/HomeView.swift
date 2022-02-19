639a2a6f93548c304f79d03f055463a39270aaf7
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-19 18:35:27 +0100

    Fix `PreviewProvider` bug

--- Cryp2p/Views/Home/HomeView.swift
+++ Cryp2p/Views/Home/HomeView.swift
@@ -53,5 +53,6 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .environmentObject(ContentModel())
     }
 }
