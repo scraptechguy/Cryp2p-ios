6d3af6697b9ec286359d785ca7660e047ae09a79
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-19 17:24:22 +0100

    Initial Commit

---
+++ Cryp2p/Cryp2pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cryp2pApp.swift
+//  Cryp2p
+//
+//  Created by Rostislav Brož on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct Cryp2pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
