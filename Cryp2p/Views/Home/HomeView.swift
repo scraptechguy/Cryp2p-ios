bdc8237486450fb90d31706efda6c994207dbc0f
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-19 19:40:02 +0100

    Expand navigation bar

--- Cryp2p/Views/Home/HomeView.swift
+++ Cryp2p/Views/Home/HomeView.swift
@@ -34,83 +34,125 @@ struct HomeView: View {
                 Spacer()
                 
                 HStack {
-                    Button(action: {
-                                withAnimation {
-                                    model.viewShown = 0
-                                }
-                            }, label: {
-                                
-                            })
-                    
                     Spacer()
                     
-                    Button(action: {
-                                withAnimation {
-                                    model.viewShown = 1
-                                }
-                            }, label: {
-                                if model.viewShown == 1 {
-                                    Image(systemName: "arrow.down.app.fill")
-                                        .resizable()
-                                        .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
-                                        .foregroundColor(model.buttonClr)
-                                } else {
-                                    Image(systemName: "arrow.down.app")
-                                        .resizable()
-                                        .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
-                                        .foregroundColor(model.buttonClr)
-                                }
-                    })
-                    
-                    Spacer()
-                    
-                    Button(action: {
-                                withAnimation {
-                                    model.viewShown = 2
-                                }
-                            }, label: {
-                                ZStack {
-                                    if model.viewShown == 2 {
-                                        Circle()
-                                            .fill()
-                                            .frame(width: model.screenSize.width / 6, height: model.screenSize.width / 6)
+                    Group {
+                        Button(action: {
+                                    withAnimation {
+                                        model.viewShown = 0
+                                    }
+                                }, label: {
+                                    if model.viewShown == 0 {
+                                        Image(systemName: "dollarsign.circle.fill")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
                                             .foregroundColor(model.buttonClr)
                                     } else {
-                                        Circle()
-                                            .fill()
-                                            .frame(width: model.screenSize.width / 6, height: model.screenSize.width / 6)
-                                            .foregroundColor(Color.pink)
+                                        Image(systemName: "dollarsign.circle")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
                                     }
-                                    
-                                    Image(systemName: "qrcode.viewfinder")
-                                        .resizable()
-                                        .frame(width: model.screenSize.width / 12, height: model.screenSize.width / 12)
-                                        .foregroundColor(model.fontClr)
-                                }
-                    }).padding([.bottom], model.screenSize.width / 15)
+                                })
+                        
+                        Spacer()
+                    }
                     
-                    Spacer()
+                    Group {
+                        Button(action: {
+                                    withAnimation {
+                                        model.viewShown = 1
+                                    }
+                                }, label: {
+                                    if model.viewShown == 1 {
+                                        Image(systemName: "arrow.down.app.fill")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
+                                    } else {
+                                        Image(systemName: "arrow.down.app")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
+                                    }
+                        })
+                        
+                        Spacer()
+                    }
                     
-                    Button(action: {
-                                withAnimation {
-                                    model.viewShown = 3
-                                }
-                            }, label: {
-                                if model.viewShown == 3 {
-                                    Image(systemName: "location.fill")
-                                        .resizable()
-                                        .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
-                                        .foregroundColor(model.buttonClr)
-                                } else {
-                                    Image(systemName: "location")
-                                        .resizable()
-                                        .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
-                                        .foregroundColor(model.buttonClr)
+                    Group {
+                        Button(action: {
+                                    withAnimation {
+                                        model.viewShown = 2
+                                    }
+                                }, label: {
+                                    ZStack {
+                                        if model.viewShown == 2 {
+                                            Circle()
+                                                .fill()
+                                                .frame(width: model.screenSize.width / 6, height: model.screenSize.width / 6)
+                                                .foregroundColor(model.buttonClr)
+                                        } else {
+                                            Circle()
+                                                .fill()
+                                                .frame(width: model.screenSize.width / 6, height: model.screenSize.width / 6)
+                                                .foregroundColor(Color.pink)
+                                        }
+                                        
+                                        Image(systemName: "qrcode.viewfinder")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 12, height: model.screenSize.width / 12)
+                                            .foregroundColor(model.fontClr)
+                                    }
+                        }).padding([.bottom], model.screenSize.width / 15)
+                        
+                        Spacer()
+                    }
+                    
+                    Group {
+                        Button(action: {
+                                    withAnimation {
+                                        model.viewShown = 3
+                                    }
+                                }, label: {
+                                    if model.viewShown == 3 {
+                                        Image(systemName: "location.fill")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
+                                    } else {
+                                        Image(systemName: "location")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
 
-                                }
-                    })
+                                    }
+                        })
+                        
+                        Spacer()
+                    }
                     
-                    Spacer()
+                    Group {
+                        Button(action: {
+                                    withAnimation {
+                                        model.viewShown = 4
+                                    }
+                                }, label: {
+                                    if model.viewShown == 4 {
+                                        Image(systemName: "gearshape.fill")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
+                                    } else {
+                                        Image(systemName: "gearshape")
+                                            .resizable()
+                                            .frame(width: model.screenSize.width / 15, height: model.screenSize.width / 15)
+                                            .foregroundColor(model.buttonClr)
+                                    }
+                        })
+                        
+                        Spacer()
+                    }
                 }.padding([.bottom], model.screenSize.width / 25)
                     .frame(width: model.screenSize.width, height: model.screenSize.width / 5)
                     .background(Rectangle()
