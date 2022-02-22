f0b3b21ce34201273474a7ae84206e81b9057e20
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-22 19:01:28 +0100

    Create functioning QR code scanner

--- Cryp2p/ViewModels/ContentModel.swift
+++ Cryp2p/ViewModels/ContentModel.swift
@@ -29,4 +29,7 @@ class ContentModel: ObservableObject {
     
     @Published var viewShown: Int = 0
     @Published var showingWalletManagerSheet: Bool = false
+    @Published var isShowingScanner: Bool = false
+    
+    @Published var phrase: String = ""
 }
--- Cryp2p/Views/Home/ChildViews/ScanView.swift
+++ Cryp2p/Views/Home/ChildViews/ScanView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CodeScanner
 
 struct ScanView: View {
     // Access data in ContentModel.swift
@@ -13,7 +14,42 @@ struct ScanView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text("Scan")
+        ZStack {
+            VStack {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "Fifa", completion: handleScan)
+            }
+            
+            Text(model.phrase)
+                .foregroundColor(.white)
+            
+            Button(action: {
+                        model.isShowingScanner = true
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(model.objectsClrMedium)
+                                .frame(width: model.screenSize.width / 2, height: model.screenSize.width / 8)
+                                .cornerRadius(model.screenSize.width / 28)
+                        
+                            Text("Start scanning")
+                                .foregroundColor(model.fontClr)
+                                .font(.system(size: model.screenSize.width / 17))
+                                .frame(width: model.screenSize.width / 1.6, height: model.screenSize.width / 8, alignment: .center)
+                        }
+            }).padding([.top], model.screenSize.width / 1.2)
+        }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        model.isShowingScanner = false
+        
+        switch result {
+            case .success(let result):
+                model.phrase = result.string
+                
+            case .failure(let error):
+                print("Scanning failed: \(error.localizedDescription)")
+        }
     }
 }
 
