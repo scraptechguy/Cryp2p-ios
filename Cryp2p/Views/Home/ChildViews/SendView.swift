0b4d666c6d1e06f3da1813a40844af2967ca6ed9
Rostislav Brož <rostislav.broz@outlook.com>
2022-02-23 17:30:56 +0100

    Create initial design of `SendView.swift`

--- Cryp2p/Views/Home/ChildViews/SendView.swift
+++ Cryp2p/Views/Home/ChildViews/SendView.swift
@@ -13,7 +13,59 @@ struct SendView: View {
     @EnvironmentObject var model: ContentModel
     
     var body: some View {
-        Text(model.phrase)
+        ScrollView(showsIndicators: false) {
+            VStack {
+                Heading(text: "Address")
+                    .padding([.top], model.screenSize.width / 4.3)
+                
+                ZStack {
+                    Rectangle()
+                        .fill(model.objectsClrMedium)
+                        .frame(width: model.screenSize.width / 1.1, height: model.screenSize.width / 6)
+                        .cornerRadius(model.screenSize.width / 15)
+                    
+                    Text(model.phrase)
+                        .foregroundColor(model.fontClr)
+                        .font(.system(size: model.screenSize.width / 20))
+                        .frame(width: model.screenSize.width  / 1.3, height: model.screenSize.width / 10, alignment: .leading)
+                }
+                
+                Heading(text: "Amount")
+                    .padding([.top], model.screenSize.width / 15)
+                
+                ZStack {
+                    Rectangle()
+                        .fill(model.objectsClrMedium)
+                        .frame(width: model.screenSize.width / 1.1, height: model.screenSize.width / 6)
+                        .cornerRadius(model.screenSize.width / 15)
+                    
+                    Text("0.1")
+                        .foregroundColor(model.fontClr)
+                        .font(.system(size: model.screenSize.width / 15))
+                        .frame(width: model.screenSize.width  / 1.6, height: model.screenSize.width / 10, alignment: .leading)
+                    
+                    Text("~ $9")
+                        .foregroundColor(model.buttonClrObscure)
+                        .font(.system(size: model.screenSize.width / 20))
+                        .frame(width: model.screenSize.width  / 1.5, height: model.screenSize.width / 10, alignment: .trailing)
+                }
+                
+                Button(action: {
+                    
+                    }, label: {
+                        ZStack {
+                            Rectangle()
+                                .fill(model.objectsClrMedium)
+                                .frame(width: model.screenSize.width / 1.7, height: model.screenSize.width / 7)
+                                .cornerRadius(model.screenSize.width / 15)
+                            
+                            Text("Send")
+                                .foregroundColor(model.fontClr)
+                                .font(.system(size: model.screenSize.width / 18))
+                        }
+                }).padding([.top], model.screenSize.width / 3)
+            }
+        }
     }
 }
 
