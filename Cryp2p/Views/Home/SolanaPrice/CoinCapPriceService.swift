8422ddb202b7d7a139544ed473bb2b1353cdf96e
Rostislav Brož <rostislav.broz@outlook.com>
2022-03-05 20:37:28 +0100

    Make some progress on live Solana price tracking

--- Cryp2p/Views/Home/SolanaPrice/CoinCapPriceService.swift
+++ Cryp2p/Views/Home/SolanaPrice/CoinCapPriceService.swift
@@ -13,4 +13,37 @@ class CoinCapPriceService: NSObject {
     private let session = URLSession(configuration: .default)
     private var wsTask: URLSessionWebSocketTask?
     
+    private let coinDictionarySubject = CurrentValueSubject<[String: Coin], Never> ([:])
+    private var coinDictionary: [String: Coin] {coinDictionarySubject.value}
+    
+    private let connectionStateSubject = CurrentValueSubject<Bool, Never> (false)
+    private var isConnected: Bool {connectionStateSubject.value}
+    
+    
+    func connect() {
+        let coins = CoinType.allCases
+            .map {$0.rawValue}
+            .joined(separator: ",")
+        
+        let url = URL(string: "wss://ws.coincap.io/prices?assets=\(coins)")!
+        wsTask = session.webSocketTask(with: url)
+        wsTask?.delegate = self
+        wsTask?.resume()
+    }
+    
+    
+    deinit {
+        coinDictionarySubject.send(completion: .finished)
+        connectionStateSubject.send(completion: .finished)
+    }
+}
+
+extension CoinCapPriceService: URLSessionWebSocketDelegate {
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        
+    }
+    
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        
+    }
 }
